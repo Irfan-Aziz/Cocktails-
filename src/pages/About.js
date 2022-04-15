ca88d9d79f152c6ce6226d7c9e15c508a175ea84
Irfan-Aziz <irfanas489@gmail.com>
2022-04-16 04:19:32 +0600

    final commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+        rerum, officiis doloremque architecto aliquam, suscipit vitae amet
+        maiores id ratione deleniti cum quod quasi sunt, neque ex? Illum, cum
+        nisi? Exercitationem vitae necessitatibus totam eaque quos consectetur
+        laudantium eos fugit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
